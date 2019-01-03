b52fef99dd953271cefc773c2a774f784bc37718
Yuhao Yang <yuhao.yang@intel.com>
2019-01-03 10:05:14 -0800

    update train

--- xray_nnclassifier/train.py
+++ xray_nnclassifier/train.py
@@ -12,7 +12,7 @@ from pyspark.storagelevel import StorageLevel
 from zoo.common.nncontext import *
 from zoo.feature.image.imagePreprocessing import *
 from zoo.feature.common import ChainedPreprocessing
-from zoo.pipeline.api.keras.layers import Input, Flatten, Dense, GlobalAveragePooling2D
+from zoo.pipeline.api.keras.layers import Input, Flatten, Dense, GlobalAveragePooling2D, Dropout
 from zoo.pipeline.api.keras.metrics import AUC
 from zoo.pipeline.api.keras.optimizers import Adam
 from zoo.pipeline.api.keras.models import Model
@@ -39,7 +39,8 @@ def get_resnet_model(model_path, label_length):
     inputNode = Input(name="input", shape=(3, 224, 224))
     inception = model.to_keras()(inputNode)
     flatten = GlobalAveragePooling2D(dim_ordering='th')(inception)
-    logits = Dense(label_length, activation="sigmoid")(flatten)
+    dropout = Dropout(0.1)(flatten)
+    logits = Dense(label_length, activation="sigmoid")(dropout)
     lrModel = Model(inputNode, logits)
     return lrModel
 
@@ -70,7 +71,7 @@ def get_densenet_model(model_path, label_length):
 
 def get_sgd_optimMethod(num_epoch, trainingCount, batchSize):
     iterationPerEpoch = int(ceil(float(trainingCount) / batchSize))
-    maxIteration = num_epoch * iterationPerEpoch
+    # maxIteration = num_epoch * iterationPerEpoch
     warmupEpoch = 10
     warmup_iteration = warmupEpoch * iterationPerEpoch
     init_lr = 1e-6
@@ -88,10 +89,37 @@ def get_sgd_optimMethod(num_epoch, trainingCount, batchSize):
     return optim
 
 
-def get_adam_optimMethod():
-    return Adam(lr=0.001,
-                schedule=Plateau("Loss", factor=0.1, patience=1, mode="min", epsilon=0.01,
-                                             cooldown=0, min_lr=1e-15))
+def get_adam_optimMethod(num_epoch, trainingCount, batchSize):
+    iterationPerEpoch = int(ceil(float(trainingCount) / batchSize))
+    warmupEpoch = 5
+    warmup_iteration = warmupEpoch * iterationPerEpoch
+    init_lr = 1e-7
+    maxlr = 0.001
+    print("peak lr is: ", maxlr)
+    warmupDelta = (maxlr - init_lr) / warmup_iteration
+    cooldownIteration = (num_epoch - warmupEpoch) * iterationPerEpoch
+
+    lrSchedule = SequentialSchedule(iterationPerEpoch)
+    lrSchedule.add(Warmup(warmupDelta), warmup_iteration)
+    lrSchedule.add(Plateau("Loss", factor=0.1, patience=1, mode="min", epsilon=0.01, cooldown=0, min_lr=1e-15),
+                   cooldownIteration)
+    optim = Adam(lr=init_lr, schedule=lrSchedule)
+    return optim
+
+
+def evaluate(testDF):
+    predictionDF = nnModel.transform(testDF).persist(storageLevel=StorageLevel.DISK_ONLY)
+    label_texts = ["Atelectasis", "Cardiomegaly", "Effusion", "Infiltration", "Mass", "Nodule", "Pneumonia",
+                   "Pneumothorax", "Consolidation",
+                   "Edema", "Emphysema", "Fibrosis", "Pleural_Thickening", "Hernia"]
+    total_auc = 0.0
+    for i in range(label_length):
+        roc_score = get_auc_for_kth_class(i, predictionDF)
+        total_auc += roc_score
+        print('{:>12} {:>25} {:>5} {:<20}'.format('roc score for ', label_texts[i], ' is: ', roc_score))
+
+    print("Finished evaluation, average auc: ", total_auc / float(label_length))
+
 
 def get_auc_for_kth_class(k, df, label_col="label", prediction_col="prediction"):
     get_Kth = udf(lambda a: a[k], DoubleType())
@@ -105,6 +133,7 @@ def get_auc_for_kth_class(k, df, label_col="label", prediction_col="prediction")
         .evaluate(extracted_df)
 
     return roc_score
+
 
 if __name__ == "__main__":
     random.seed(1234)
@@ -124,13 +153,15 @@ if __name__ == "__main__":
 
     xray_model = get_resnet_model(model_path, label_length)
     train_df = spark.read.load(data_path + '/trainingDF')
-    split_id = '00000040_000.png'  #00026868_000.png
+    split_id = '00000040_000.png'  #'00024779_000.png'
     trainingDF = train_df.filter(train_df["Image_Index"] < split_id)
     validationDF = train_df.filter(train_df["Image_Index"] >= split_id)
     trainingCount = trainingDF.count()
+    print("num of training images: ", trainingCount)
+    print("num of validation images: ", validationDF.count())
 
     transformer = ChainedPreprocessing(
-        [RowToImageFeature(), ImageRandomPreprocessing(ImageHFlip(), 0.5),
+        [RowToImageFeature(), ImageCenterCrop(224, 224), ImageRandomPreprocessing(ImageHFlip(), 0.5),
          ImageChannelNormalize(123.68, 116.779, 103.939), ImageMatToTensor(), ImageFeatureToTensor()])
 
     train_summary = TrainSummary(log_dir="./logs", app_name="test_dell_x_ray")
@@ -138,6 +169,8 @@ if __name__ == "__main__":
     train_summary.set_summary_trigger("LearningRate", SeveralIteration(1))
     train_summary.set_summary_trigger("Loss", SeveralIteration(1))
 
+    optim_method = get_adam_optimMethod(num_epoch, trainingCount, batch_size)
+    # get_sgd_optimMethod(num_epoch, trainingCount, batch_size)
     classifier = NNEstimator(xray_model, BinaryCrossEntropy(), transformer) \
         .setBatchSize(batch_size) \
         .setMaxEpoch(num_epoch) \
@@ -146,13 +179,9 @@ if __name__ == "__main__":
         .setValidation(EveryEpoch(), validationDF, [AUC()], batch_size) \
         .setTrainSummary(train_summary) \
         .setValidationSummary(val_summary) \
-        .setOptimMethod(get_adam_optimMethod())
+        .setOptimMethod(optim_method)
 
-        # .setOptimMethod(get_adam_optimMethod())
-        # .setOptimMethod(get_sgd_optimMethod(num_epoch, trainingCount, batch_size))
-        # .setLearningRate(1e-1)
         # .setEndWhen(MaxIteration(1))
-        # .setOptimMethod(SGD(learningrate=0.001, leaningrate_schedule=Plateau("Loss", factor=0.1, patience=1, mode="min", epsilon=0.01, cooldown=0, min_lr=1e-15))) \
         # .setCheckpoint("./checkpoints", EveryEpoch(), False)
 
     start = time.time()
@@ -162,20 +191,14 @@ if __name__ == "__main__":
     model_path = save_path + '/xray_model_' + time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime())
     nnModel.model.saveModel(model_path + ".bigdl", model_path + ".bin", True)
     print('model saved at: ', model_path)
+
+    print("evaluating on training data: ")
+    evaluate(trainingDF)
     SQLContext(sc).clearCache()
 
+    print("\nevaluating on test data: ")
     testDF = spark.read.load(data_path + '/testDF')
-    predictionDF = nnModel.transform(testDF).persist(storageLevel=StorageLevel.DISK_ONLY)
-    label_texts = ["Atelectasis", "Cardiomegaly", "Effusion", "Infiltration", "Mass", "Nodule", "Pneumonia",
-                   "Pneumothorax", "Consolidation",
-                   "Edema", "Emphysema", "Fibrosis", "Pleural_Thickening", "Hernia"]
-    total_auc = 0.0
-    for i in range(label_length):
-        roc_score = get_auc_for_kth_class(i, predictionDF)
-        total_auc += roc_score
-        print('{:>12} {:>25} {:>5} {:<20}'.format('roc score for ', label_texts[i], ' is: ', roc_score))
-
-    print("Finished evaluation, average auc: ", total_auc / float(label_length))
+    evaluate(testDF)
 
 
     # nnModel.model.to_model().saveModel("/home/yuhao/workspace/github/hhbyyh/BigDL-ImageProcessing-Examples/xray_nnclassifier/kerasModel/m.bigdl",
