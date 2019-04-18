02963310a46fdf0e58a2db3a11ddae8229273ce7
leela_uppuluri <leela_uppuluri@dell.com>
2019-04-18 09:09:54 -0500

    Updated files recent changes

--- chestXray_webApp/console.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/lib/zoo/bin/spark-submit-with-zoo.sh --master local[2] --num-executors 2 --executor-cores 8 --driver-memory 40g --executor-memory 40g /home/leelau/chestXray/webApp_Ver2/chestxray_webapp.py
-
-#/usr/lib/zoo/bin/spark-submit-with-zoo.sh --master local[2] --num-executors 2 --executor-cores 8 --driver-memory 40g --executor-memory 40g /home/leelau/chestXray/webApp/xrayWebApp.py
